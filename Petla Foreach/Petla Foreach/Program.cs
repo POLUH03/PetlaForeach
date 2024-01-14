f0c043ecf3e994fc9b4486cb56720f2836b5fae9
POLUH03 <80423162+POLUH03@users.noreply.github.com>
2024-01-14 15:43:47 +0100

    Add files via upload

---
+++ Petla Foreach/Petla Foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] pets = { "kot", "pies", "papuga", "świnka morska" };
+
+        foreach (string zwierze in pets)
+        {
+            if (zwierze == "pies")
+            {
+                Console.WriteLine("Koniec");
+                continue;
+            }
+
+            Console.WriteLine(zwierze);
+
+            Console.ReadKey();
+        }
+    }
+}
